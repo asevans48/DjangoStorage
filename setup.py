38eca012cb19b0c541a9e5a05401a991e8d5ee28
aevans <aevans48@simplrsinsites.com>
2018-10-08 15:56:50 -0600

    update

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(name='Distutils',
-      version='0.1a',
-      description='Storage module for Django',
-      author='Andrew Evans',
-      author_email='aevans48@simplrinsites.com',
-      packages=['storage',],
-     )
