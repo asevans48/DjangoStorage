578575db8604b8a4562294e276f8ff647fdc5619
aevans <aevans48@simplrsinsites.com>
2018-10-08 16:12:54 -0600

    udpate

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Django Storage',
+      version='0.1a',
+      description='Storage module for django',
+      author='Andrew Evans',
+      author_email='aevans48@simplrinsites.com',
+      packages=['storage',],
+     )
